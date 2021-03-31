9840353a052766f3b5b5a3d936cc52648065bd7f
Sabbir Alam <sabbir.nit@gmail.com>
2021-03-31 21:23:35 +0800

    testing

--- src/main/java/com/example/jpa/h2/sample/SampleSpringbootJpaApplication.java
+++ src/main/java/com/example/jpa/h2/sample/SampleSpringbootJpaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleSpringbootJpaApplication {
-
+	//Main method
 	public static void main(String[] args) {
 		SpringApplication.run(SampleSpringbootJpaApplication.class, args);
 	}
