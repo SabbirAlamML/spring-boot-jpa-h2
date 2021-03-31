5da13bab930bad749deb2d201476b85ba2628e9c
Sabbir Alam <sabbir.nit@gmail.com>
2021-03-31 20:35:37 +0800

    added first file

---
+++ src/main/java/com/example/jpa/h2/sample/SampleSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa.h2.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpa/h2/sample/SampleSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.h2.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
